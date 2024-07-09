9deffb4ed1d6045dee91b798ec6249a7d232009b
gema_handika <gemahandika88@gmail.com>
2024-07-09 10:31:16 +0700

    update

--- public/views/asset/modal.php
+++ public/views/asset/modal.php
@@ -1,93 +1,64 @@
-<?php
-include '../../../app/config/koneksi.php';
+<!DOCTYPE html>
+<html lang="en">
 
-?>
-<style>
-    .result {
-        background-color: green;
-        color: #fff;
-        padding: 20px;
-    }
-
-    .row {
-        display: flex;
-    }
-</style>
-<!-- Modal Create -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <form action="../../../app/controller/Anggota.php" method="post">
-            <div class="modal-content">
-                <div class="modal-header btn btn-success">
-                    <h6 class="modal-title fs-5" id="exampleModalLabel">Tambah Data Anggota</h6>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <div class="report-it">
-                        <input type="hidden" id="report1" name="password" value="123" required onkeyup="myFunction()">
-                        <input type="hidden" id="status" name="status_user" value="NON AKTIF" readonly>
-                        <input type="hidden" id="pemasukan" name="pemasukan" value="pemasukan" readonly>
-                        <input type="hidden" id="keterangan" name="keterangan" value="iuran bulanan anggota">
-
-                        <div class="form-group">
-                            <label id="resultDisplay" style="width:300px">Scan qr code: </label><br>
-                            <div id="resultDisplay">Result Here</div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="nama_anggota">Nomor Asset :</label><br>
-                            <input class="form-control" type="text" id="nama_anggota" name="nama_anggota">
-                            <!-- <input class="form-control" type="text" id="nama_anggota" name="nama_anggota"> -->
-                        </div>
-                        <div class="form-group">
-                            <label for="nip">NIP :</label><br>
-                            <input class="form-control" type="text" id="nip" name="nip">
-                        </div>
-                        <div class="form-group">
-                            <label for="join_date">Join Date :</label><br>
-                            <input class="form-control" type="date" id="join_date" name="join_date" value="<?= $date ?>">
-                        </div>
-                        <div class="form-group">
-                            <label for="divisi">Divisi :</label><br>
-                            <input class="form-control" type="text" id="divisi" name="divisi">
-                        </div>
-                        <div class="form-group">
-                            <label for="cabang">Cabang :</label><br>
-                            <input class="form-control" type="text" id="cabang" name="cabang">
-                        </div>
-
-                        <div class="form-group">
-                            <label for="saldo">Saldo :</label><br>
-                            <input class="form-control" type="text" id="saldo" name="saldo" value=0 onkeypress="return inputAngka(event)">
-                        </div>
-                        <input type="hidden" id="status" name="status" value="AKTIF">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-success" name="add">Create</button>
-                </div>
-            </div>
-        </form>
-    </div>
-</div>
-
-<script src="html5-qrcode.min.js"></script>
-<script type="text/javascript">
-    function onScanSuccess(qrCodeMessage) {
-        document.getElementById('resultDisplay').innerHTML = '<span class="result">' + qrCodeMessage + '</span>';
-        document.getElementById('nama_anggota').value = qrCodeMessage;
-    }
-
-    function onScanError(errorMessage) {
-        // handle scan error
-    }
-
-    var html5QrcodeScanner = new Html5QrcodeScanner(
-        "reader", {
-            fps: 10,
-            qrbox: 250
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QR Code Scanner</title>
+    <script src="html5-qrcode.min.js"></script>
+    <style>
+        .result {
+            background-color: green;
+            color: #fff;
+            padding: 20px;
         }
-    );
-    html5QrcodeScanner.render(onScanSuccess, onScanError);
-</script>
+
+        .row {
+            display: flex;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="row">
+        <div class="col">
+            <div style="width:500px;" id="reader"></div>
+        </div>
+        <div class="col" style="padding:30px;">
+            <div id="resultDisplay">Result Here</div>
+            <div class="form-group">
+                <label for="nama_anggota">Nama Anggota :</label><br>
+                <input class="form-control" type="text" id="nama_anggota" name="nama_anggota">
+            </div>
+            <div class="form-group">
+                <label for="nama_anggota">Nama Anggota :</label><br>
+                <input class="form-control" type="text" id="nama_anggota" name="nama_anggota">
+            </div>
+            <div class="form-group">
+                <label for="nama_anggota">Nama Anggota :</label><br>
+                <input class="form-control" type="text" id="nama_anggota" name="nama_anggota">
+            </div>
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        function onScanSuccess(qrCodeMessage) {
+            document.getElementById('resultDisplay').innerHTML = '<span class="result">' + qrCodeMessage + '</span>';
+            document.getElementById('nama_anggota').value = qrCodeMessage;
+        }
+
+        function onScanError(errorMessage) {
+            // handle scan error
+        }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            "reader", {
+                fps: 10,
+                qrbox: 250
+            }
+        );
+        html5QrcodeScanner.render(onScanSuccess, onScanError);
+    </script>
+</body>
+
+</html>
